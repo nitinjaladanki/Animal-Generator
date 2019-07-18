b71f723c376f7a1efe8e0553117128356a23f6a8
Nitin Jaladanki <nitin.jaladanki@gmail.com>
2019-07-18 10:18:48 -0700

    Animal Generator Final

--- Animal Generator/ViewController.swift
+++ Animal Generator/ViewController.swift
@@ -9,12 +9,22 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var animalHead: UIImageView!
+    @IBOutlet weak var animalBody: UIImageView!
+    
+    var headImages = ["Cat Head", "Cow Head", "Donkey Head", "Elephant Head", "Lion Head", "Monkey Head", "Moose Head", "Panda Head", "Pig Head", "Sheep Head", "Tiger Head", "Zebra Head"]
+    
+    var bodyImages = ["Zebra Body", "Tiger Body", "Sheep Body", "Pig Body", "Panda Body", "Moose Body", "Monkey Body", "Lion Body", "Elephant Body", "Donkey Body", "Cow Body", "Cat Body"]
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
+    
+    
+    @IBAction func bodyPicker(_ sender: Any)
+    {
+        let picker = Int.random(in: 0...11)
+        animalHead.image = UIImage(named: headImages[picker])
+        animalBody.image = UIImage(named: bodyImages[picker])
 }
-
+}
